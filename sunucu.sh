5405411af9f1d8d6ba306887e38edf9a61760b77
milisman <milisarge@gmail.com>
2016-10-16 22:40:37 +0300

    acilis

--- sunucu.sh
+++ sunucu.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
 ./port_oldur.sh
-python2 sunucu.py
+python2 sunucu.py &
+
+if which xdg-open > /dev/null
+then
+  xdg-open "http://127.0.0.1:6060"
+elif which gnome-open > /dev/null
+then
+  gnome-open "http://127.0.0.1:6060"
+fi
+
