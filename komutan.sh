b5a2c28eed07bab430ac0b4242ffef539e1c19f2
milisarge <milisarge@gmail.com>
2017-03-20 05:39:29 +0200

    gunicorn-zamanasim

--- komutan.sh
+++ komutan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./port_oldur.sh
-sudo gunicorn -w 2 -b 127.0.0.1:6060 sunucu:app &
+sudo gunicorn -w 2 -b 127.0.0.1:6060 sunucu:app -t 240 &
 sleep 2
 if which xdg-open > /dev/null
 then
