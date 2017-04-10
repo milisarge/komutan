e87251394eec968d1626a3ded7376186de3256af
milisarge <milisarge@gmail.com>
2017-04-10 15:52:39 +0300

    chat.eklenti

--- test.py
+++ test.py
@@ -5,5 +5,15 @@ from arge import *
 import tempfile
 import filecmp
 import subprocess
+import binascii
+from imgurpython import ImgurClient
 
-print "---",arger.komutCalistir("curl -s www &> /tmp/link_kontrol")
+client_id = '873f9d40aa94a1d'
+client_secret = '8e80e7eb2ce5559a225505993a2cc8f668e28ca8'
+
+client = ImgurClient(client_id, client_secret)
+
+# Example request
+path="static/exit.png"
+rid=client.upload_from_path(path, config=None, anon=True)
+print rid["link"]
